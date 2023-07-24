2b9ba70ac9c09bc54f79a6df91b99962e0e171f2
rocktohq <itzmonir@gmail.com>
2023-07-25 00:04:41 +0600

    Initial Update

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                almostRed: "#F00",
+                almostTomato: "#FF8938",
+            }
+        }
+    }
+}
